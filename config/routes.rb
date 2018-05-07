0584afc8ee6cf57722805be60ca93d942f8923ed
Sava Vuckovic <vuckovicsava11@gmail.com>
2018-05-07 15:44:40 +0200

    fixed root paths

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "home#index"
-
   devise_scope :user do
     authenticated :user do
       root 'home#index', as: :authenticated_root
     end
 
     unauthenticated do
-      root 'devise/sessions#new', as: :unauthenticated_root
+      root 'home#welcome', as: :unauthenticated_root
     end
   end
 end
