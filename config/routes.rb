a27e9086672a8174bd20540233e82529c95b45ce
Sava Vuckovic <vuckovicsava11@gmail.com>
2018-05-07 14:52:11 +0200

    navbar links shown dynamically

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "home#index"
+
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
