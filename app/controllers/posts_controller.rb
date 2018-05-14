c89b0f0d223acab5788fd9f1f17cee070dc95e81
Sava Vuckovic <vuckovicsava11@gmail.com>
2018-05-14 09:25:30 +0200

    refactored a callback

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,8 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   # GET /posts/1
   def show
+    @post ||= Post.find(params[:id])
   end
 
   # GET /posts/new
@@ -12,6 +12,7 @@ class PostsController < ApplicationController
 
   # GET /posts/1/edit
   def edit
+    @post ||= Post.find(params[:id])
   end
 
   # POST /posts
@@ -31,6 +32,7 @@ class PostsController < ApplicationController
 
   # PATCH/PUT /posts/1
   def update
+    @post ||= Post.find(params[:id])
     respond_to do |format|
       if @post.update(post_params)
         format.html { redirect_to @post, notice: 'Post was successfully updated.' }
@@ -44,6 +46,7 @@ class PostsController < ApplicationController
 
   # DELETE /posts/1
   def destroy
+    @post ||= Post.find(params[:id])
     @post.destroy
     respond_to do |format|
       format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
@@ -52,11 +55,6 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
-
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
       params.fetch(:post, {})
