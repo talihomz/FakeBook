c78e6bcb74c13c9fe168acebcf7caafac1b8324f
Kevin Wahome <wahome@talicraft.com>
2018-05-21 11:16:26 +0300

    Implemented User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # validates :firstname, presence:true, length: { maximum: 50 }
-  # validates :lastname, presence:true, length: { maximum: 50 }
-  # validates :bio, presence:true, length: { minimum: 10, maximum: 300 }
+  validates :firstname, presence:true, length: { maximum: 50 }
+  validates :lastname, presence:true, length: { maximum: 50 }
+  validates :bio, presence:true, length: { minimum: 10, maximum: 300 }
 
-  # has_many :posts
-  # has_many :notifications
-  # has_many :friends
+  has_many :posts
+  has_many :notifications
+  has_many :friends
 end
