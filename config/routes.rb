3e40a408c47e29384d008f170e3e4f01d32bbe4d
Kevin Wahome <wahome@talicraft.com>
2018-05-29 13:51:31 +0300

    Minor fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,17 @@
 Rails.application.routes.draw do
   resources :posts
 
+  devise_for :user, :controllers => {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
   get '/users/all', to: 'user#all', as: :find_friends
   get '/users/:id', to: 'user#show', as: :user
   get '/notifications/', to: 'notification#index', as: :notifications
   get '/privacy_policy', to: 'static_pages#privacy', as: :privacy
 
   match "*path", to: "application#render_404", via: :all
-
-  devise_for :user, :controllers => {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations',
-    passwords: 'users/passwords',
-    confrimations: 'users/confirmations'
-  }
 
   devise_scope :user do
     authenticated :user do
