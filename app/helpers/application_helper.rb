f5eb08fcd9c86620085254004a029496dd3bb7e1
Kevin Wahome <wahome@talicraft.com>
2018-05-31 01:30:20 +0300

    Added app helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,15 @@ module ApplicationHelper
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
+
+  def flash_class(level)
+    case level
+        when :notice then "alert alert-info"
+        when :success then "alert alert-success"
+        when :error then "alert alert-error"
+        when :alert then "alert alert-error"
+    end
+
+    return ""
+  end
 end
