3fc37e5708138d3cc3bcc21509727864f5c7ff3e
Kevin Wahome <wahome@talicraft.com>
2018-06-11 11:10:36 +0300

    Fixed post deleting

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   validates :content, presence: true, length: { maximum: 500 }
 
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :likes
   has_many :notifications
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180611075432) do
+ActiveRecord::Schema.define(version: 20180608073053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,9 +79,7 @@ ActiveRecord::Schema.define(version: 20180611075432) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
-  add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
   add_foreign_key "notifications", "users"
   add_foreign_key "posts", "users"
