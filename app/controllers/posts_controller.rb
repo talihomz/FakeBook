a19792eb5b06f79e62b506b07ec94314fda84b6d
Sava Vuckovic <vuckovicsava11@gmail.com>
2018-06-11 09:51:05 +0200

    being able to delete posts from everywhere

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -42,6 +42,7 @@ class PostsController < ApplicationController
 
   # DELETE /posts/1
   def destroy
+    post = Post.find(params[:id])
     post.destroy
     redirect_to authenticated_root_path, notice: 'Post was successfully destroyed.'
   end
