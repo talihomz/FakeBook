dbbedf489414277da6298ed7f95322e83044d065
Kevin Wahome <wahomekevin@gmail.com>
2018-06-14 11:53:03 +0300

    Added policy logic to Find Friends view

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -2,4 +2,8 @@ module UserHelper
   def others
     users = User.where.not(id: current_user.id)
   end
+
+  def get_friend_request(user_id)
+    FriendRequest.where("user_id = ? AND friend_id = ?", current_user.id, user_id).first || FriendRequest.new
+  end
 end
